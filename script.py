d8ed5bd105281d4bd8479de971d7d16f843ec98b
bains <suhail.singh.bains@hotmail.com>
2018-03-23 20:34:52 +0530

    Search a particular entity and Insert at a particular position

--- script.py
+++ script.py
@@ -1,25 +1,5 @@
-#Trip Calculator 
-def hotel_cost(nights):
-  return 140*nights
-def plane_ride_cost(city):
-  if(city=="Charlotte"):
-    return 183
-  elif(city == "Tampa"):
-    return 220
-  elif(city =="Pittsburgh"):
-    return 222
-  elif(city=="Los Angeles"):
-    return 475
-  else :
-  	return 0
-def rental_car_cost(days):
-    if(days>=7):
-      return 40*days - 50
-    elif(days>=3):
-      return 40*days - 20
-    else:
-      return 40*days
-def trip_cost(city,days,spending_money):
-  return rental_car_cost(days)+hotel_cost(days - 1)+plane_ride_cost(city)+spending_money
-
-print trip_cost("Los Angeles", 5, 600)
+#Search a particular entity and Insert at a particular position 
+animals = ["aardvark", "badger", "duck", "emu", "fennec fox"]
+duck_index = animals.index("duck")
+animals.insert(duck_index, "cobra")
+print  animals
