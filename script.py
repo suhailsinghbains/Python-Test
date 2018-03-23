d62b4fe79d8d073c70845e29d9b9354929e985d3
bains <suhail.singh.bains@hotmail.com>
2018-03-23 19:56:43 +0530

    Made a Trip Calculator

--- script.py
+++ script.py
@@ -1,14 +1,25 @@
-# Enter Time and It will greet you
-ch = raw_input("Enter Time: ")
-if ch<'0000':
-    print("Invalid Time")
-elif ch<'1200':
-    print("Good Morning!")
-elif ch<'1600':
-    print("Good Afternoon!")
-elif ch<'2000':
-    print("Good Everning!")
-elif ch<='2400':
-    print("Good Night!")
-else:
-    print("Invalid Time")
+#Trip Calculator 
+def hotel_cost(nights):
+  return 140*nights
+def plane_ride_cost(city):
+  if(city=="Charlotte"):
+    return 183
+  elif(city == "Tampa"):
+    return 220
+  elif(city =="Pittsburgh"):
+    return 222
+  elif(city=="Los Angeles"):
+    return 475
+  else :
+  	return 0
+def rental_car_cost(days):
+    if(days>=7):
+      return 40*days - 50
+    elif(days>=3):
+      return 40*days - 20
+    else:
+      return 40*days
+def trip_cost(city,days,spending_money):
+  return rental_car_cost(days)+hotel_cost(days - 1)+plane_ride_cost(city)+spending_money
+
+print trip_cost("Los Angeles", 5, 600)
