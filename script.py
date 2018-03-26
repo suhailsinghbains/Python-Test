ecbed04a49db29918c226a1e86e2386319708d35
bains <suhail.singh.bains@hotmail.com>
2018-03-26 23:34:01 +0530

    Shopping Cart

--- script.py
+++ script.py
@@ -1,14 +1,25 @@
-#Messing Name with FOR Loop
-Name = raw_input("Enter your Name: ")
-print("H")
-print("i")
-print
-for letter in Name:
-  print letter
+#Shopping Cart
+shopping_list = ["banana", "orange", "apple"]
 
-print
-print
-word = "Just Messing"
+stock = {
+"banana": 6,
+"apple": 0,
+"orange": 32,
+"pear": 15
+}
 
-for letter in word:
-    print letter
+prices = {
+"banana": 4,
+"apple": 2,
+"orange": 1.5,
+"pear": 3
+}
+
+def compute_bill(food):
+total = 0
+for item in food:
+  if(stock[item]>0):
+    total += prices[item]
+  stock[item] -= 1
+return total
+print compute_bill(['apple', 'banana'])
