1593ee11ba1fb07ed3f1475c1466641ce4c7640d
suhailsinghbains <suhail.singh.bains@hotmail.com>
2018-03-29 15:07:53 -0600

    BattleShip Game

--- script.py
+++ script.py
@@ -1,48 +1,45 @@
-#Find Class Average
-students = [ alice, lloyd, tyler]
-lloyd = {
-  "name": "Lloyd",
-  "homework": [90.0, 97.0, 75.0, 92.0],
-  "quizzes": [88.0, 40.0, 94.0],
-  "tests": [75.0, 90.0]
-}
-alice = {
-  "name": "Alice",
-  "homework": [100.0, 92.0, 98.0, 100.0],
-  "quizzes": [82.0, 83.0, 91.0],
-  "tests": [89.0, 97.0]
-}
-tyler = {
-  "name": "Tyler",
-  "homework": [0.0, 87.0, 75.0, 22.0],
-  "quizzes": [0.0, 75.0, 78.0],
-  "tests": [100.0, 100.0]
-}
-def average(numbers):
-  total = float(sum(numbers))
-  total /= len(numbers)
-  return total;
-def get_average(Stu):
-  homework = average(Stu["homework"])
-  quizzes = average(Stu["quizzes"])
-  tests = average(Stu["tests"])
-  sum = (homework*10 + quizzes*30 + tests*60)/100.
-  return sum
-def get_letter_grade(score):
-  if score>=90:
-    return "A"
-  elif score>=80:
-    return "B"
-  elif score>=70:
-    return "C"
-  elif score>=60:
-    return "D"
+#BattleShip!
+from random import randint
+
+board = []
+
+for x in range(0, 5):
+  board.append(["O"] * 5)
+
+def print_board(board):
+  for row in board:
+    print " ".join(row)
+
+print_board(board)
+
+def random_row(board):
+  return randint(0, len(board) - 1)
+
+def random_col(board):
+  return randint(0, len(board[0]) - 1)
+
+ship_row = random_row(board)
+ship_col = random_col(board)
+print ship_row
+print ship_col
+
+for turn in range(4):
+  print "Turn", turn + 1
+  guess_row = int(raw_input("Guess Row: "))
+  guess_col = int(raw_input("Guess Col: "))
+
+  if guess_row == ship_row and guess_col == ship_col:
+    print "Congratulations! You sank my battleship!"
+    break
   else:
-    return "F"
-def get_class_average(class_list):
-  results = []
-  for Stu in class_list:
-    results.append(get_average(Stu))
-  return average(results)
-print get_class_average(students)
-print get_letter_grade(get_class_average(students))
+    if guess_row not in range(5) or \
+      guess_col not in range(5):
+      print "Oops, that's not even in the ocean."
+    elif board[guess_row][guess_col] == "X":
+      print( "You guessed that one already." )
+    else:
+      print "You missed my battleship!"
+      board[guess_row][guess_col] = "X"
+    if(turn==3) :
+      print "Game Over"
+    print_board(board)
