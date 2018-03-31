4fc6129275184bf0eef1f1b0b96c85d1fa7bb4fb
bainsPC <suhail.singhyo@gmail.com>
2018-04-01 00:17:45 +0530

    Simple Game

--- script.py
+++ script.py
@@ -1,9 +1,12 @@
-#Loops Finally
-count = 0
-
-if count < 5:
-  print "Hello, I am an if statement and count is", count
-
-while count < 10:
-  print "Hello, I am a while and count is", count
-  count += 1
+#Simple Game
+from random import randint
+random_number = randint(1, 10)
+guesses_left = 3
+while guesses_left>0:
+  guess = raw_input("Enter a value b/w 1 & 10")
+  if(guess==random_number):
+    print "You win!"
+    break
+  guesses_left-=1
+else:
+    print "You lose."
