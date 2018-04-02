5c4553923af75e8876be8ea9171045345e620f4c
bainsPC <suhail.singhyo@gmail.com>
2018-04-02 11:15:53 +0530

    Counting App

--- script.py
+++ script.py
@@ -1,12 +1,5 @@
-#Simple Game
-from random import randint
-random_number = randint(1, 10)
-guesses_left = 3
-while guesses_left>0:
-  guess = raw_input("Enter a value b/w 1 & 10")
-  if(guess==random_number):
-    print "You win!"
-    break
-  guesses_left-=1
-else:
-    print "You lose."
+#Counting
+print "Counting..."
+
+for i in range(20):
+  print i
