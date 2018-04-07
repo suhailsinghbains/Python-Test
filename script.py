d15d855b781a1c7b48bbe41b417805eb3167493c
bainsPC <suhail.singhyo@gmail.com>
2018-04-07 12:35:12 +0530

    Censorship is Important!

--- script.py
+++ script.py
@@ -1,9 +1,27 @@
-# Reversing using Loop
-def reverse(text):
-  i = len(text)
-  newText = ""
-  while(i>0):
-    newText+=text[i-1]
-    i-=1
-  return newText
-print (reverse("HI"))
+#Censorship comes into action
+def censor(text, word):
+  l = len(text)
+  i=0
+  obj=[]
+  test=[]
+  final=""
+  while(i<l):
+      test.append(text[i])
+      if(text[i:i+len(word)]==word):
+          j=0
+          while(j<len(word)):
+              obj.append(i+j)
+              j+=1
+      i+=1
+  print (obj)
+  k=0
+  while (k<l):
+      if k in obj:
+          test[k]="*"
+      k+=1
+  k=0
+  while(k<l):
+    final+=str(test[k])
+    k+=1
+  return final
+print (censor("this hack is wack hack", "hack"))
