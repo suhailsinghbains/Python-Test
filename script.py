caafd641900285cdc19b9bb7efc444ec86e9d7de
bainsPC <suhail.singhyo@gmail.com>
2018-04-08 23:54:13 +0530

    Conting in an Object

--- script.py
+++ script.py
@@ -1,27 +1,7 @@
-#Censorship comes into action
-def censor(text, word):
-  l = len(text)
-  i=0
-  obj=[]
-  test=[]
-  final=""
-  while(i<l):
-      test.append(text[i])
-      if(text[i:i+len(word)]==word):
-          j=0
-          while(j<len(word)):
-              obj.append(i+j)
-              j+=1
-      i+=1
-  print (obj)
-  k=0
-  while (k<l):
-      if k in obj:
-          test[k]="*"
-      k+=1
-  k=0
-  while(k<l):
-    final+=str(test[k])
-    k+=1
-  return final
-print (censor("this hack is wack hack", "hack"))
+# Counting in Object
+def count(sequence, item):
+  count=0;
+  for num in sequence:
+    if(num==item):
+      count+=1;
+  return count
