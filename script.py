4809c8f996ef23e60c599f11078863f20ed20d40
bainsPC <suhail.singhyo@gmail.com>
2018-04-14 19:01:22 +0530

    Printing Result

--- script.py
+++ script.py
@@ -3,7 +3,7 @@ grades = [100, 100, 90, 40, 80, 100, 85, 70, 90, 65, 90, 85, 50.5]
 
 def print_grades(grades_input):
   for grade in grades_input:
-    print grade
+    print (grade)
 
 def grades_sum(scores):
   total = 0
@@ -12,8 +12,8 @@ def grades_sum(scores):
   return total
 
 def grades_average(grades_input):
-  sum_of_grades = grades_sum(gradeen(grades_input))
-  return average
+  sum_of_grades = grades_sum(grades_input)
+  return sum_of_grades/len(grades_input)
 
 def grades_variance(scores):
     average = grades_average(scores)
@@ -23,3 +23,12 @@ def grades_variance(scores):
     return variance / len(scores)
 def grades_std_deviation(variance):
   return variance**0.5
+
+#Print all Stuff
+for item in grades:
+  print (item)
+print (grades_sum(grades))
+print (grades_average(grades))
+variance = grades_variance(grades)
+print (variance)
+print (grades_std_deviation(variance))
