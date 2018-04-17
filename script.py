bcfc5e61de9406555cb30c40aa37ffd740f716c1
suhailsinghbains <suhail.singh.bains@hotmail.com>
2018-04-17 20:09:08 +0530

    Filter

--- script.py
+++ script.py
@@ -1,3 +1,4 @@
-#MoreConditions
-squares = [x**2 for x in range(1,11)]
-print (filter(lambda x: x>=30 and x<=70, squares))
+#Filter
+garbled = "IXXX aXXmX aXXXnXoXXXXXtXhXeXXXXrX sXXXXeXcXXXrXeXt mXXeXsXXXsXaXXXXXXgXeX!XX"
+message = filter(lambda x: x !='X', garbled)
+print (message)
