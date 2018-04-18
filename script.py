811e316dfab403b47d73c9c593fe9269dc307a1c
suhailsinghbains <suhail.singh.bains@hotmail.com>
2018-04-18 22:29:16 +0530

    Binary Numbers

--- script.py
+++ script.py
@@ -1,4 +1,13 @@
-#Filter
-garbled = "IXXX aXXmX aXXXnXoXXXXXtXhXeXXXXrX sXXXXeXcXXXrXeXt mXXeXsXXXsXaXXXXXXgXeX!XX"
-message = filter(lambda x: x !='X', garbled)
-print (message)
+#Binary
+one = 0b1
+two = 0b10
+three = 0b11
+four = 0b100
+five = 0b101
+six  = 0b110
+seven = 0b111
+eight = 0b1000
+nine = 0b1001
+ten = 0b1010
+eleven = 0b1011
+twelve = 0b1100
