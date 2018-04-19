a7a8bf88dea0ffc2c987b9b531d2ccc198838409
bainsPC <suhail.singhyo@gmail.com>
2018-04-19 20:29:46 +0530

    Automagically Binary

--- script.py
+++ script.py
@@ -1,13 +1,5 @@
-#Binary
-one = 0b1
-two = 0b10
-three = 0b11
-four = 0b100
-five = 0b101
-six  = 0b110
-seven = 0b111
-eight = 0b1000
-nine = 0b1001
-ten = 0b1010
-eleven = 0b1011
-twelve = 0b1100
+#Auto
+print bin(2)
+print bin(3)
+print bin(4)
+print bin(5)
