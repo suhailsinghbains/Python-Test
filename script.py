9a00564e4ad10121d586a82eaaab047224acc38f
suhailsinghbains <suhail.singh.bains@hotmail.com>
2018-04-29 22:46:18 +0530

    Adding Health Var

--- script.py
+++ script.py
@@ -2,8 +2,15 @@
 class Animal(object):
   """Makes cute animals."""
   is_alive = True
+  health = "good"
   def __init__(self, name, age):
     self.name = name
     self.age = age
   def description(name,age):
   	print name,age
+hippo = Animal("Jake", 12)
+sloth = Animal("Yo" ,13)
+ocelot = Animal("Yo1", 14)
+print hippo.health
+print sloth.health
+print ocelot.health
