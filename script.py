30292234323b3b4920797b859fb256d212233f4a
suhailsinghbains <suhail.singh.bains@hotmail.com>
2018-05-01 14:38:58 +0530

    Implementing Inheritence

--- script.py
+++ script.py
@@ -1,24 +1,12 @@
-#First Ever Class Under Construction
-class ShoppingCart(object):
-  """Creates shopping cart objects
-  for users of our fine website."""
+#inheritence
+class Shape(object):
+  """Makes shapes!"""
+  def __init__(self, number_of_sides):
+    self.number_of_sides = number_of_sides
 
-  def __init__(self, customer_name):
-    self.customer_name = customer_name
-    self.items_in_cart = {"Banana"}
-  def add_item(self, product, price):
-    """Add product to the cart."""
-    if not product in self.items_in_cart:
-      self.items_in_cart[product] = price
-      print product + " added."
-    else:
-      print product + " is already in the cart."
-
-  def remove_item(self, product):
-    """Remove product from the cart."""
-    if product in self.items_in_cart:
-      del self.items_in_cart[product]
-      print product + " removed."
-    else:
-      print product + " is not in the cart."
-my_cart = ShoppingCart("Suhail")
+# Add your Triangle class below!
+class Triangle(Shape):
+  def __init__(self, side1,side2,side3):
+    self.side1= side1
+    self.side2= side2
+    self.side3= side3
