99b066a35d7e44f175ecee408adefc3f4581b08a
suhailsinghbains <suhail.singh.bains@hotmail.com>
2018-05-01 14:56:16 +0530

    Trianlge Class Stuff

--- script.py
+++ script.py
@@ -1,20 +1,16 @@
-#Super keyword
-class Employee(object):
-  """Models real-life employees!"""
-  def __init__(self, employee_name):
-    self.employee_name = employee_name
+#Making Triangle
+class Triangle(object):
+  number_of_sides = 3
+  def __init__(self, angle1, angle2, angle3):
+    self.angle1 = angle1
+    self.angle2 = angle2
+    self.angle3 = angle3
+  def check_angles(self):
+    if(self.angle1+self.angle2+self.angle3==180):
+      return True
+    else:
+      return False
 
-  def calculate_wage(self, hours):
-    self.hours = hours
-    return hours * 20.00
-
-# Add your code below!
-class PartTimeEmployee(Employee):
-  def calculate_wage(self, hours):
-    self.hours = hours
-    return hours * 12.00
-  def full_time_wage(self, hours):
-    return super(PartTimeEmployee, self).calculate_wage(hours)
-
-milton = PartTimeEmployee("Suhail")
-print milton.calculate_wage(10)
+my_triangle = Triangle(90,30,60)
+print my_triangle.number_of_sides
+print my_triangle.check_angles()
