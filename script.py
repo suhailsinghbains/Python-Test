7b1c883d01439b5b7a77905f2c4291379c5c32a8
suhailsinghbains <suhail.singh.bains@hotmail.com>
2018-05-01 14:41:18 +0530

    More Inheritence

--- script.py
+++ script.py
@@ -1,12 +1,15 @@
-#inheritence
-class Shape(object):
-  """Makes shapes!"""
-  def __init__(self, number_of_sides):
-    self.number_of_sides = number_of_sides
+#More Inheritence
+class Employee(object):
+  """Models real-life employees!"""
+  def __init__(self, employee_name):
+    self.employee_name = employee_name
 
-# Add your Triangle class below!
-class Triangle(Shape):
-  def __init__(self, side1,side2,side3):
-    self.side1= side1
-    self.side2= side2
-    self.side3= side3
+  def calculate_wage(self, hours):
+    self.hours = hours
+    return hours * 20.00
+
+# Add your code below!
+class PartTimeEmployee(Employee):
+  def calculate_wage(self, hours):
+    self.hours = hours
+    return hours * 12.00
