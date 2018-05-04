0e233002da9884588661c8710f926d1b8b87f26d
suhailsinghbains <suhail.singh.bains@hotmail.com>
2018-05-04 22:19:07 +0530

    Continuing Building Car

--- script.py
+++ script.py
@@ -1,5 +1,10 @@
 #New Class Car
 class Car(object):
   condition = "new"
-my_car = Car()
+  def __init__(self, model, color, mpg):
+    self.model = model
+    self.color = color
+    self.mpg = mpg
+
+my_car = Car("DeLorean", "silver", 88)
 print my_car.condition
