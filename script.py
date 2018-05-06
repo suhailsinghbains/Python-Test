f58b6035659b26d6db909015b414015b16a6e042
suhailsinghbains <suhail.singh.bains@hotmail.com>
2018-05-06 21:20:55 +0530

    Continuing Building Car

--- script.py
+++ script.py
@@ -4,9 +4,14 @@ class Car(object):
   def __init__(self, model, color, mpg):
     self.model = model
     self.color = color
-    self.mpg = mpg
+    self.mpg   = mpg
+  def display_car(self):
+    x = "This is a "+ self.color +" "+ self.model +" with "+ str(self.mpg) +" MPG."
+    return x
 
 my_car = Car("DeLorean", "silver", 88)
+
 print my_car.model
 print my_car.color
 print my_car.mpg
+print my_car.display_car()
