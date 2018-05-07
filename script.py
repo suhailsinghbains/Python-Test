73ae10a84ef8b802aaac717617adad432dcd2c58
suhailsinghbains <suhail.singh.bains@hotmail.com>
2018-05-07 22:14:16 +0530

    Continuing Building Car

--- script.py
+++ script.py
@@ -8,10 +8,14 @@ class Car(object):
   def display_car(self):
     x = "This is a "+ self.color +" "+ self.model +" with "+ str(self.mpg) +" MPG."
     return x
+  def drive_car(self):
+    self.condition = "used"
 
 my_car = Car("DeLorean", "silver", 88)
 
 print my_car.model
 print my_car.color
 print my_car.mpg
-print my_car.display_car()
+print my_car.condition
+my_car.drive_car()
+print my_car.condition
