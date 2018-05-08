737022e9661e19ad30d9d1584cd765e9aff7f859
suhailsinghbains <suhail.singh.bains@hotmail.com>
2018-05-08 21:51:43 +0530

    Continuing Building Car

--- script.py
+++ script.py
@@ -5,17 +5,18 @@ class Car(object):
     self.model = model
     self.color = color
     self.mpg   = mpg
+
   def display_car(self):
-    x = "This is a "+ self.color +" "+ self.model +" with "+ str(self.mpg) +" MPG."
-    return x
+    print "This is a %s %s with %s MPG." % (self.color, self.model, str(self.mpg))
+
   def drive_car(self):
     self.condition = "used"
 
-my_car = Car("DeLorean", "silver", 88)
+class ElectricCar(Car):
+  def __init__(self, model, color, mpg, battery_type):
+    self.model = model
+    self.color = color
+    self.mpg   = mpg
+    self.battery_type = battery_type
 
-print my_car.model
-print my_car.color
-print my_car.mpg
-print my_car.condition
-my_car.drive_car()
-print my_car.condition
+my_car = ElectricCar("DeLorean", "silver", 88, "molten salt")
