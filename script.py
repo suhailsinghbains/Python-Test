b024c0350367b62e33d92eb75057d568b45f18f7
suhailsinghbains <suhail.singh.bains@hotmail.com>
2018-05-12 08:01:08 +0530

    Learning Files

--- script.py
+++ script.py
@@ -1,10 +1,2 @@
-# Files
-my_list = [i ** 2 for i in range(1, 11)]
-# Generates a list of squares of the numbers 1 - 10
-
-f = open("output.txt", "w")
-
-for item in my_list:
-  f.write(str(item) + "\n")
-
-f.close()
+# Uderstanding Files
+my_file = open("output.txt", "r+")
